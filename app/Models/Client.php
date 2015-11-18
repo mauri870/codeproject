355f802818e4a78db3923d4ffc2f4d0c74883354
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-18 20:26:34 -0200

    Moved models to Models Folder

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Codeproject;
+namespace Codeproject\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Codeproject;
+namespace Codeproject\Models;
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
