3c7078adfce560b28cdf4effe6cd64179832065a
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-23 19:31:48 -0200

    Added methods create and update to service

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -9,7 +9,24 @@
 namespace Codeproject\Services;
 
 
+use Codeproject\Repositories\ClientRepository;
+
 class ClientService
 {
+    protected $repository;
 
+    public function __construct(ClientRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function create(array $data)
+    {
+        return $this->repository->create($data);
+    }
+
+    public function update(array $data, $id)
+    {
+        return $this->repository->update($data, $id);
+    }
 }
