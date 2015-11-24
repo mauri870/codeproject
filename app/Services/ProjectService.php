7318489b0885be3d817015d3d97d521e70d83be9
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-24 11:42:57 -0200

    Added methods for project service

--- app/Services/ProjectService.php
+++ app/Services/ProjectService.php
@@ -8,19 +8,59 @@
 
 namespace Codeproject\Services;
 
-
-use Codeproject\Repositories\ClientRepository;
+use Codeproject\Repositories\ProjectRepository;
+use Codeproject\Validators\ProjectValidator;
+use Prettus\Validator\Exceptions\ValidatorException;
 
 class ProjectService
 {
     /**
-     * @var ClientRepository
+     * @var ProjectRepository
      */
-    private $repository;
+    protected $repository;
+    /**
+     * @var ProjectValidator
+     */
+    private $validator;
 
-    public function __construct(ClientRepository $repository)
+    public function __construct(ProjectRepository $repository, ProjectValidator $validator)
     {
         $this->repository = $repository;
+        $this->validator = $validator;
+    }
+
+    public function create(array $data)
+    {
+        try{
+            $this->validator->with($data)->passesOrFail();
+
+            return $this->repository->create($data);
+
+        } catch(ValidatorException $e){
+
+            return [
+                'error' => true,
+                'message' => $e->getMessageBag(),
+            ];
+
+        }
+    }
+
+    public function update($data, $id)
+    {
+        try{
+            $this->validator->with($data)->passesOrFail();
+
+            return $this->repository->update($data,$id);
+
+        } catch(ValidatorException $e){
+
+            return [
+                'error' => true,
+                'message' => $e->getMessageBag(),
+            ];
+
+        }
     }
 
 
