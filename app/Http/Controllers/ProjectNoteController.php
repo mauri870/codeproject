9b11eaaa054dd4fd897a31e5ecb6ee3bfc0c1705
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-25 18:55:12 -0200

    Create controller for project notes

---
+++ app/Http/Controllers/ProjectNoteController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Codeproject\Http\Controllers;
+
+use Codeproject\Repositories\ClientRepository;
+use Codeproject\Repositories\ProjectRepository;
+use Codeproject\Services\ProjectService;
+use Illuminate\Http\Request;
+
+use Codeproject\Http\Requests;
+use Codeproject\Http\Controllers\Controller;
+
+class ProjectController extends Controller
+{
+    /**
+     * @var ClientRepository
+     */
+    private $repository;
+    /**
+     * @var ProjectService
+     */
+    private $service;
+
+    public function __construct(ProjectRepository $repository, ProjectService $service)
+    {
+        $this->repository = $repository;
+        $this->service = $service;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       return $this->repository->with(['user','client'])->all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return $this->service->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $this->repository->find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return $this->service->update($request->all(), $id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return $this->repository->delete($id);
+    }
+}
