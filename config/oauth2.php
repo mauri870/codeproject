cff20a2a5cfa90447c75390b958ffa9d066b2007
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-26 20:04:29 -0200

    Fixed wrong parameter on config array

--- config/oauth2.php
+++ config/oauth2.php
@@ -31,7 +31,7 @@ return [
         'password' => [
             'class' => League\OAuth2\Server\Grant\PasswordGrant::class,
             'callback' => 'Codeproject\OAuth\PasswordVerifier@verify',
-            'acess_token_ttl' => 3600
+            'access_token_ttl' => 3600
         ]
     ],
 
