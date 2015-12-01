fb1a8c81af363c1542d0d01deb75fcfa60ee6187
Mauri de Souza Nunes <mauri870@gmail.com>
2015-12-01 21:27:03 -0200

    Create entity for project tasks

---
+++ app/Entities/ProjectTask.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Codeproject\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class ProjectTask extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    protected $fillable = [
+        'name',
+        'project_id',
+        'start_date',
+        'due_date',
+        'due_date',
+        'status',
+    ];
+
+
+    /**
+     * Relation between tasks and projects
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function project()
+    {
+        return $this->belongsTo(Project::class,'project_id');
+    }
+}
