742208fbf55cc98d543365f4120f62ff9eeb7c28
Mauri de Souza Nunes <mauri870@gmail.com>
2015-12-01 21:37:57 -0200

    Add repository layer

---
+++ app/Repositories/ProjectTaskRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Codeproject\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface ProjectTaskRepository
+ * @package namespace Codeproject\Repositories;
+ */
+interface ProjectTaskRepository extends RepositoryInterface
+{
+    //
+}
---
+++ app/Repositories/ProjectTaskRepositoryEloquent.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Codeproject\Repositories;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+use Codeproject\Repositories\ProjectTaskRepository;
+use Codeproject\Entities\ProjectTask;
+
+/**
+ * Class ProjectTaskRepositoryEloquent
+ * @package namespace Codeproject\Repositories;
+ */
+class ProjectTaskRepositoryEloquent extends BaseRepository implements ProjectTaskRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return ProjectTask::class;
+    }
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+}
