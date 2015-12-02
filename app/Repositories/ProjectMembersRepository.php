9a8cbb2298b3a93776af729c5a45fd6c16ef0bc4
Mauri de Souza Nunes <mauri870@gmail.com>
2015-12-02 10:04:17 -0200

    Create ProjectMembers entity

---
+++ app/Repositories/ProjectMembersRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Codeproject\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface ProjectMembersRepository
+ * @package namespace Codeproject\Repositories;
+ */
+interface ProjectMembersRepository extends RepositoryInterface
+{
+    //
+}
