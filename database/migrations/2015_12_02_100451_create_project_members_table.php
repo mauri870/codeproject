3bd96d52f627b87ed92da2d2f20cc27b055971f2
Mauri de Souza Nunes <mauri870@gmail.com>
2015-12-02 12:23:05 -0200

    Add timestamps and id

--- database/migrations/2015_12_02_100451_create_project_members_table.php
+++ database/migrations/2015_12_02_100451_create_project_members_table.php
@@ -14,10 +14,12 @@ class CreateProjectMembersTable extends Migration
     public function up()
     {
         Schema::create('project_members', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('project_id')->unsigned();
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade')->onUpdate('cascade');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
         });
     }
 
